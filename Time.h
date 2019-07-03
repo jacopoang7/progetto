e575f94e72bd6caa57a9d7ee4304acf61b31a61c
jacopoang7 <52325232+jacopoang7@users.noreply.github.com>
2019-07-03 22:11:51 +0200

    Create Time.h

---
+++ Time.h
@@ -0,0 +1,29 @@
+#ifndef PROGETTO_TIME_H
+#define PROGETTO_TIME_H
+
+
+class Time {
+private:
+    int hours;
+    int minutes;
+public:
+    Time(){
+        hours=0;
+        minutes=0;
+    }
+    Time(int h, int m):hours(h),minutes(m){}
+    ~Time(){}
+    void SetNewTime(int hours,int minutes);
+    int GetHours();
+    int GetMinutes();
+    void SetHours(int hours);
+    void SetMinutes(int minutes);
+    void GetCurrentTime();
+    void PrintTime();
+
+
+
+};
+
+
+#endif //PROGETTO_TIME_H
