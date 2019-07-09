49e4debee267f2a7aa154c3c0c09854a6a9bb5b6
jacopoang7 <52325232+jacopoang7@users.noreply.github.com>
2019-07-09 18:43:03 +0200

    Create Countdown.h

---
+++ Countdown.h
@@ -0,0 +1,38 @@
+#ifndef COUNTDOWN_H
+#define COUNTDOWN_H
+
+#include <QWidget>
+class QSpinBox;
+class QLCDNumber;
+class QPushButton;
+class QTimer;
+
+class Countdown : public QWidget
+{
+  Q_OBJECT
+public:
+explicit Countdown(QWidget *base = nullptr);
+int Scount, Mcount, Hcount;
+QSpinBox *casella1;
+QSpinBox *casella2;
+QSpinBox *casella3;
+QTimer *updatetimer;
+QLCDNumber *lCDNumber1;
+QLCDNumber *lCDNumber2;
+QLCDNumber *lCDNumber3;
+
+
+
+private slots:
+void click_bottone1();
+void click_bottone2();
+void update();
+
+private:
+ QPushButton *bottone1;
+ QPushButton *bottone2;
+// QTimer *updatetimer;
+
+};
+
+#endif // COUNTDOWN_H
