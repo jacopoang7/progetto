65c6865fba3f372926f17b6d11ac6300323d0e0b
jacopoang7 <52325232+jacopoang7@users.noreply.github.com>
2019-07-09 20:52:11 +0200

    Create TimeS.cpp

---
+++ TimeS.cpp
@@ -0,0 +1,34 @@
+#include "TimeS.h"
+void Time::SetNewTime(int hours,int minutes,int seconds){
+    this->hours=hours;
+    this->minutes=minutes;
+    this->seconds=seconds;
+}
+int TimeS::GetHours() {
+    return hours;
+}
+int TimeS::GetMinutes() {
+    return minutes;
+}
+int TimeS::GetSeconds() {
+    return seconds;
+}
+void TimeS::SetHours(int hours) {
+    this->hours=hours;
+}
+void TimeS::SetMinutes(int minutes) {
+    this->minutes=minutes;
+}
+void TimeS::SetSeconds(int seconds) {
+    this->seconds=seconds;
+}
+void TimeS::GetCurrentTime() {
+    time_t now = time(0);
+    tm *ltm = localtime(&now);
+    hours=1 + ltm->tm_hour;
+    minutes=1 + ltm->tm_min;
+    seconds=1 + 1tm->tm_sec;
+}
+void TimeS::PrintTime() {
+    cout<<"Time="<<hours<<":"<<minutes<<":"<<seconds<<endl;
+}
