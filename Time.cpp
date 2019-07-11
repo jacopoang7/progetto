b8e837315ba4f8a19802356d692bb732e49386a6
jacopoang7 <52325232+jacopoang7@users.noreply.github.com>
2019-07-11 21:02:47 +0200

    Update Time.cpp

--- Time.cpp
+++ Time.cpp
@@ -14,9 +14,11 @@ int Time::GetMinutes() {
     return minutes;
 }
 void Time::SetHours(int hours) {
+    if(hours<25 && hours>0)
     this->hours=hours;
 }
-void Time::SetMinutes(int minutes) {
+void Time::SetMinutes(int minutes){
+    if(minutes>-1 && minutes<61)
     this->minutes=minutes;
 }
 void Time::GetCurrentTime() {
@@ -26,5 +28,15 @@ void Time::GetCurrentTime() {
     minutes=1 + ltm->tm_min;
 }
 void Time::PrintTime() const {
+    if(hours>0 && hours<25 && minutes>-1 && minutes<60)
     std::cout<<"Time="<<hours<<":"<<minutes<<std::endl;
 }
+void Time:PrintTimeEnglish() const{
+    if(hours>0 && hours<25 && minutes>-1 && minutes<60) {
+        if (hours <= 12) {
+            std::cout << "Time=" << hours << ":" << minutes << "am" << std::endl;
+        } else {
+            std::cout << "Time=" << hours << ":" << minutes << "pm" << std::endl;
+        }
+    }
+};
