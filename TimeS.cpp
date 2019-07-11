050618990dfd1a32b2d590d78ec2097b103df8ef
jacopoang7 <52325232+jacopoang7@users.noreply.github.com>
2019-07-11 20:18:54 +0200

    Update TimeS.cpp

--- TimeS.cpp
+++ TimeS.cpp
@@ -1,26 +1,15 @@
 #include<ctime>
 #include<iostream>
 #include "TimeS.h"
-void TimeS::SetNewTime(int hours,int minutes,int seconds){
+void TimeS::SetNewTime(int hours,int minutes,int seconds) {
     this->hours=hours;
     this->minutes=minutes;
     this->seconds=seconds;
-}
-int TimeS::GetHours() {
-    return hours;
-}
-int TimeS::GetMinutes() {
-    return minutes;
-}
+}}
 int TimeS::GetSeconds() {
     return seconds;
 }
-void TimeS::SetHours(int hours) {
-    this->hours=hours;
-}
-void TimeS::SetMinutes(int minutes) {
-    this->minutes=minutes;
-}
+
 void TimeS::SetSeconds(int seconds) {
     this->seconds=seconds;
 }
