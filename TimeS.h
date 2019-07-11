8bd2f1fa700c3794a91740d35e7a94ebcffa60ec
jacopoang7 <52325232+jacopoang7@users.noreply.github.com>
2019-07-11 21:03:40 +0200

    Update TimeS.h

--- TimeS.h
+++ TimeS.h
@@ -1,26 +1,36 @@
-#ifndef PROGETTO_TIMES_H
-#define PROGETTO_TIMES_H
-#include "Time.h"
 
-class TimeS: public Time {
-private:
-    int hours;
-    int minutes;
-    int seconds;
-public:
-    TimeS():Time(){
-        seconds=0;
+#include<ctime>
+#include<iostream>
+#include "TimeS.h"
+void TimeS::SetNewTime(int hours,int minutes,int seconds) {
+    this->hours=hours;
+    this->minutes=minutes;
+    this->seconds=seconds;
+}}
+int TimeS::GetSeconds() {
+    return seconds;
+}
+
+void TimeS::SetSeconds(int seconds) {
+    this->seconds=seconds;
+}
+void TimeS::GetCurrentTime() {
+    time_t now = time(0);
+    tm *ltm = localtime(&now);
+    hours=1 + ltm->tm_hour;
+    minutes=1 + ltm->tm_min;
+    seconds=1 + 1tm->tm_sec;
+}
+void TimeS::PrintTime() const{
+    if(hours>0 && hours<25 && minutes>-1 && minutes<60 && seconds>-1 && seconds<61 )
+    std::cout<<"Time="<<hours<<":"<<minutes<<":"<<seconds<<std::endl;
+}
+void TimeS::PrintTimeEnglish() const {
+    if(hours>0 && hours<25 && minutes>-1 && minutes<60 && seconds>-1 && seconds<61 ){
+        if (hours <= 12) {
+            std::cout << "Time=" << hours << ":" << minutes <<":"<<seconds<< "am" << std::endl;
+        } else {
+            std::cout << "Time=" << hours << ":" << minutes <<":"<<seconds<< "pm" << std::endl;
+        }
     }
-    TimeS(int h, int m, int s):Time(h,m), seconds(s){}
-    virtual ~TimeS(){}
-    void SetNewTime(int hours,int minutes,int seconds) override;
-    int GetSeconds();
-    void SetSeconds(int seconds);
-    void GetCurrentTime() override;
-    void PrintTime() const override;
-    void PrintTimeEnglish() const override;
-
-};
-
-
-#endif //PROGETTO_TIMES_H
+}
