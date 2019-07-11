a830dab80ac8c58751ce85fec12aeabccecf138d
jacopoang7 <52325232+jacopoang7@users.noreply.github.com>
2019-07-11 17:26:46 +0200

    Create DateTimeS.h

---
+++ DateTimeS.h
@@ -0,0 +1,18 @@
+
+#ifndef PROGETTO_DATETIMES_H
+#define PROGETTO_DATETIMES_H
+
+#include"Date.h"
+#include "TimeS.h"
+
+class DateTimeS :public Date, public TimeS{
+public:
+    DateTimeS():Date(),TimeS(){}
+    DateTimeS(int d,int m,int y,int h, int min,int sec):Day(d),Month(m),Year(y),hours(h),minutes(min),seconds(sec){}
+    virtual ~DateTimeS(){}
+    void PrintDateTimeS() const;
+
+};
+
+
+#endif //PROGETTO_DATETIMES_H
