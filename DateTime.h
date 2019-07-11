8009b5f9b1f5f03eeef3d8518ebba6c8ebc8e4b4
jacopoang7 <52325232+jacopoang7@users.noreply.github.com>
2019-07-11 17:25:44 +0200

    Update DateTime.h

--- DateTime.h
+++ DateTime.h
@@ -1,16 +1,15 @@
-
 #ifndef PROGETTO_DATETIME_H
 #define PROGETTO_DATETIME_H
 
 #include "Time.h"
 #include "Date.h"
-#include<ctime>
+
 class DateTime: public Date, public Time {
 public:
     DateTime():Date(),Time(){}
     DateTime(int d,int m,int y,int h, int min):Day(d),Month(m),Year(y),hours(h),minutes(min){}
-    ~DateTime(){}
-    void PrintDateTime();
+    virtual ~DateTime(){}
+    void PrintDateTime() const;
 };
 
 
